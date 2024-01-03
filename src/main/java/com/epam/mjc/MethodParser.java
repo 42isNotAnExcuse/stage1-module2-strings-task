8ba47faa1eefece9cca11d01377c732b99ae290f
Igor Bugaiov <bugayov.igor@nixsolutions.com>
2024-01-03 11:06:09 +0100

    st1_mod2_t_strings_try_1

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,54 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        MethodSignature result;
+        List<MethodSignature.Argument> argList = new ArrayList<>();
+        String rawArgsStr;
+        String argsStr;
+        String methName;
+        String[] rawSign;
+        String[] parts = signatureString.split("\\(");
+
+        rawArgsStr = parts[1].trim();
+        argsStr = rawArgsStr.substring(0, rawArgsStr.length() - 1);
+
+        if (!argsStr.isEmpty()) {
+            for (String argPair : argsStr.split(",")) {
+                String type = argPair.trim().split(" ")[0];
+                String name = argPair.trim().split(" ")[1];
+                argList.add(new MethodSignature.Argument(type, name));
+            }
+        }
+
+        rawSign = parts[0].split(" ");
+        methName = rawSign[rawSign.length - 1];
+        result = argsStr.isEmpty() ? new MethodSignature(methName) : new MethodSignature(methName, argList);
+        result.setReturnType(rawSign[rawSign.length - 2]);
+        if (rawSign.length > 2)
+            result.setAccessModifier(rawSign[0]);
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -2,17 +2,29 @@ package com.epam.mjc;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringSplitter {
 
     /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     * Splits given string applying all delimiters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        String resStr = source;
+
+        for (String c : delimiters.toArray(new String[0])) {
+            resStr = resStr.replaceAll(c, " ");
+        }
+
+        return Stream.of(resStr.trim()
+                        .replaceAll("\\s\\s+", " ")
+                        .split(" "))
+                .collect(Collectors.toList());
     }
 }
